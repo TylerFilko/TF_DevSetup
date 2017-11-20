24bf8c59255707ca4a7fd34a3d8906792888f660
Tyler Filko <tafilko@msp-0986.spscommerce.com>
2017-11-20 15:46:32 -0600

    Adding features to CompDevSetup.py
    Continuing to add to the document, working out the bugs associated with the rc files (python and input). Now both files are linked to the local repo so when updates are pulled from github the functionality is automatically added to the users machine.

--- CompDevSetup.py
+++ CompDevSetup.py
@@ -10,36 +10,37 @@ Need to download Github repo before beginning (save in home)
 import subprocess
 
 def mac_dev_setup():
-    # #Install Homebrew to usr directory
-    # try:
-    #     print("Updating Homebrew")
-    #     subprocess.run(['brew', 'update'],check=True)
-    # except subprocess.CalledProcessError:
-    #     print("installing Homebrew")
-    #     subprocess.run('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"', shell=True,check=True)
-    #
-    # print("Installing pyenv")
-    # subprocess.run(['brew', 'install', 'pyenv'])
-    #
-    # print("Install Pyenv-virtualenv")
-    # subprocess.run(['brew', 'install', 'pyenv-virtualenv'])
-    #
-    # print("Install direnv")
-    # subprocess.run(['brew', 'install', 'direnv'])
+    #Install Homebrew to usr directory
+    try:
+        print("Updating Homebrew")
+        subprocess.run(['brew', 'update'],check=True)
+    except subprocess.CalledProcessError:
+        print("installing Homebrew")
+        subprocess.run('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"', shell=True,check=True)
+
+    print("Installing pyenv")
+    subprocess.run(['brew', 'install', 'pyenv'])
+
+    print("Install Pyenv-virtualenv")
+    subprocess.run(['brew', 'install', 'pyenv-virtualenv'])
+
+    print("Install direnv")
+    subprocess.run(['brew', 'install', 'direnv'])
 
     #Copy and save pythonrc.py file on usr home
     # print('Creating pythonrc.py simlink')
     # subprocess.check_call(['ln', '-s', '/Users/tafilko/DevelopmentSetup/pythonrc.py', '/Users/tafilko/pythonrc.py'])
-    #
+    subprocess.check_call(['ln', '-s', '/Users/tafilko/DevelopmentSetup/inputrc', '/etc/.inputrc'])
     #Updating Bash profile
     #Except catches if user doesn't have access to .bash_profile file, updates so user has read/write ability
     print("Updating .bash_profile")
-    #subprocess.run(['cat', '/Users/tafilko/', '|','pbcopy'])
-    # try:
-    subprocess.run(['cat bash_profile_template.txt >> /Users/tafilko/.bash_profile'], shell=True, check=True)
-    # except subprocess.CalledProcessError:
-    #     subprocess.run(['sudo chmod 664 .bash_profile'],shell=True,check=True)
-    #     subprocess.run(['cat','bash_update.txt' '>>', '/User/tafilko/.bash_profile'],check=True)
+    # TODO:
+    subprocess.run(['cat', '/Users/tafilko/', '|','pbcopy'])
+    try:
+        subprocess.run(['pdpaste', '>>', '/User/tafilko/.bash_profile'],check=True)
+    except subprocess.CalledProcessError:
+        subprocess.run(['sudo chmod 664', '.bash_profile'],check=True)
+        subprocess.run(['pdpaste', '>', '/User/tafilko/.bash_profile'])
 
     print("you are all set to go")
 
