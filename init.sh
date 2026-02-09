415bbe6158bb41064e6783bb3cf83e7154b5becc
Tyler Filko <tfilko@redhat.com>
2026-02-09 10:42:59 -0700

    removal of old python install scrips

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 # Setup scrip updates
 
 # Brew install ----------------------------------
-yes | brew install asdf coreutils git delve docker terraform tflint
+yes | brew install asdf coreutils git delve docker terraform tflint openshift-cli gh awscli kustomize
 yes | brew install --cask visual-studio-code
 yes | brew install --cask insomnia
 
-# dev tooling install
+# dev tooling install ---------------------------
 yes | brew install kubectl derailed/k9s/k9s kind
 kubectl version --client
 
@@ -17,9 +17,29 @@ asdf install golang latest
 asdf plugin add golangci-lint
 asdf install golangci-lint latest
 
+asdf plugin add python
+asdf python latest
 
-asdf reshim golang # defur to finish fo asdf add calls
+asdf plugin add direnv
+asdf install direnv
+
+# defur to finish fo asdf add calls -------------
+asdf reshim golang 
+asdf reshim 
+
+# ZSH plugings ----------------------------------
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Env setup for ZSH -----------------------------
-cat zshprofile_template.txt >> ~/.zshprofile # once per login
-cat zshrc_teamplate.txt >> ~/.zshrc           # once per interactive shell
+cat zshprofile.template.txt >> ~/.zshprofile # once per login
+cat zshrc.template.txt >> ~/.zshrc           # once per interactive shell
+
+# Optional Installs ----------------------------------
+
+read -rp 'install Kind? (y/n) : ' kindInstallBool
+if [[ $kindInstallBool == "y" ]]
+    yes | brew install kind
+else
+    echo "Will not install Kind"
+fi
